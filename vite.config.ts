1c3cbb6bc88596dffa429c3f8ae5adf522731856
panchalyuvraj125 <panchalyuvraj125@users.noreply.github.com>
2026-06-18 18:24:45 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VoxReminder/',
   plugins: [
     react(),
     tailwindcss(),
